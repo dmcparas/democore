97d889a1c41909098c4ecf7d4758f4f51f9e2bc7
divyaprakash-tf <divya.prakash@candescent.com>
2025-11-18 08:20:29 +0530

    add grpc with kafka

--- src/main/java/com/example/core/grpc/UserGrpcServiceImpl.java
+++ src/main/java/com/example/core/grpc/UserGrpcServiceImpl.java
@@ -8,11 +8,21 @@ import com.example.core.grpc.UserRequest;
 import com.example.core.grpc.UserResponse;
 import com.example.core.grpc.UserServiceGrpc;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import java.io.FileOutputStream;
 
 @GrpcService
 public class UserGrpcServiceImpl extends UserServiceGrpc.UserServiceImplBase {
+
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    public UserGrpcServiceImpl(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
 
     @PostConstruct
     public void init() {
@@ -73,4 +83,60 @@ public class UserGrpcServiceImpl extends UserServiceGrpc.UserServiceImplBase {
             }
         };
     }
+
+    @Override
+    public StreamObserver<FileUploadRequest> uploadFileToKafka(StreamObserver<FileUploadResponse> responseObserver) {
+
+        return new StreamObserver<>() {
+            String filename;
+
+            @Override
+            public void onNext(FileUploadRequest request) {
+                try {
+                    if (filename == null) {
+                        filename = request.getFilename();
+                    }
+
+                    byte[] chunk = request.getContent().toByteArray();
+
+                    // Send chunk to Kafka
+                    kafkaTemplate.send(topicName, filename, chunk)
+                            .whenComplete((result, ex) -> {
+                                if (ex != null) {
+                                    responseObserver.onError(ex);
+                                }
+                            });
+
+
+                } catch (Exception e) {
+                    responseObserver.onError(e);
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                t.printStackTrace();
+            }
+
+            @Override
+            public void onCompleted() {
+                try {
+                    // Ensure messages are flushed
+                    kafkaTemplate.flush();
+
+                    responseObserver.onNext(
+                            FileUploadResponse.newBuilder()
+                                    .setSuccess(true)
+                                    .setMessage("File uploaded to Kafka successfully: " + filename)
+                                    .build()
+                    );
+                    responseObserver.onCompleted();
+
+                } catch (Exception e) {
+                    responseObserver.onError(e);
+                }
+            }
+        };
+    }
+
 }
